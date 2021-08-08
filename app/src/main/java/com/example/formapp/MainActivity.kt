e0d45e26c2e85c27ffb52c05b58750d8ca639cd2
brianA1998 <brian.alaniz@hotmail.com>
2021-08-08 03:24:51 -0300

    Utilizando viewbinding

--- app/src/main/java/com/example/formapp/MainActivity.kt
+++ app/src/main/java/com/example/formapp/MainActivity.kt
@@ -5,15 +5,18 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.formapp.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputEditText
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -27,7 +30,8 @@ class MainActivity : AppCompatActivity() {
         //Controlamos que elemento del menu se selecciona
         if (item.itemId == R.id.action_send) {
             val name: String = findViewById<TextInputEditText>(R.id.editTextName).text.toString()
-            Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
+            val surname = binding.editTextLastName.text.toString()
+            Toast.makeText(this, "$name $surname", Toast.LENGTH_SHORT).show()
         }
 
         return super.onOptionsItemSelected(item)
