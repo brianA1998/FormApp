1a1f91c5201bb29e96da1dec7f8099206f6d26c1
brianA1998 <brian.alaniz@hotmail.com>
2021-08-08 02:42:46 -0300

    Agregando el evento de click

--- app/src/main/java/com/example/formapp/MainActivity.kt
+++ app/src/main/java/com/example/formapp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.formapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,6 +18,21 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        /**
+         * Controlamos que elemento del menu se selecciona
+         */
+        if (item.itemId == R.id.action_send) {
+
+            Toast.makeText(this, "Click en send", Toast.LENGTH_SHORT).show()
+        }
+
+
+
+        return super.onOptionsItemSelected(item)
     }
 
 
