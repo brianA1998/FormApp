8fb28c683a7cdbd491f3dd6a710be27b5b416d90
brianA1998 <brian.alaniz@hotmail.com>
2021-08-10 20:10:09 -0300

    Agregando validacion de el campo nombre

--- app/src/main/java/com/example/formapp/MainActivity.kt
+++ app/src/main/java/com/example/formapp/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -28,31 +29,60 @@ class MainActivity : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
-
         //Controlamos que elemento del menu se selecciona
-        if (item.itemId == R.id.action_send) {
-            val name: String = findViewById<TextInputEditText>(R.id.editTextName).text.toString()
-            val surname = binding.editTextLastName.text.toString()
-            //Toast.makeText(this, "$name $surname", Toast.LENGTH_SHORT).show()
+        if (item?.itemId == R.id.action_send) {
 
 
-            //Instanciamos alertdialog
-            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-            builder.setTitle(getString(R.string.dialog_title))
-            builder.setMessage("$name  $surname")
-            builder.setPositiveButton(getString(R.string.dialog_ok),DialogInterface.OnClickListener { dialog, which ->
-                Toast.makeText(this, "Positive button", Toast.LENGTH_SHORT).show()
-            })
-            builder.setNegativeButton(getString(R.string.dialog_cancel), DialogInterface.OnClickListener { dialog, which ->
-                Toast.makeText(this,"Negative button",Toast.LENGTH_SHORT).show()
-            })
+            if (validField()) {
+                val name: String =
+                    findViewById<TextInputEditText>(R.id.editTextName).text.toString()
+                val surname = binding.editTextLastName.text.toString()
+                //Toast.makeText(this, "$name $surname", Toast.LENGTH_SHORT).show()
 
-            val dialog: AlertDialog = builder.create()
-            dialog.show()
+
+                //Instanciamos alertdialog
+                val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                builder.setTitle(getString(R.string.dialog_title))
+                builder.setMessage("$name  $surname")
+                builder.setPositiveButton(
+                    getString(R.string.dialog_ok),
+                    DialogInterface.OnClickListener { dialog, which ->
+                        Toast.makeText(this, "Positive button", Toast.LENGTH_SHORT).show()
+                    })
+                builder.setNegativeButton(
+                    getString(R.string.dialog_cancel),
+                    DialogInterface.OnClickListener { dialog, which ->
+                        Toast.makeText(this, "Negative button", Toast.LENGTH_SHORT).show()
+                    })
+
+                val dialog: AlertDialog = builder.create()
+                dialog.show()
+
+            }
 
         }
 
-        return super.onOptionsItemSelected(item)
+        return super.onOptionsItemSelected(item ?: return false)
+    }
+
+
+    /**
+     * Esta función nos permite saber si los campos obligatorios estan vacios
+     */
+    private fun validField(): Boolean {
+        var isValid = true
+
+
+        if (binding.editTextName.text.toString().length == 0) {
+            isValid = false
+        }
+
+
+
+
+
+        return isValid
+
     }
 
 
