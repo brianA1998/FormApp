3e605de5b71634eed424073df1175171b1c7c092
brianA1998 <brian.alaniz@hotmail.com>
2021-08-11 01:47:45 -0300

    Funcion de validacion de apellido terminada

--- app/src/main/java/com/example/formapp/MainActivity.kt
+++ app/src/main/java/com/example/formapp/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : AppCompatActivity() {
 
 
             if (validField()) {
+
                 val name: String =
                     findViewById<TextInputEditText>(R.id.editTextName).text.toString()
                 val surname = binding.editTextLastName.text.toString()
@@ -50,14 +51,11 @@ class MainActivity : AppCompatActivity() {
                         Toast.makeText(this, "Positive button", Toast.LENGTH_SHORT).show()
                     })
                 builder.setNegativeButton(
-                    getString(R.string.dialog_cancel),
-                    DialogInterface.OnClickListener { dialog, which ->
-                        Toast.makeText(this, "Negative button", Toast.LENGTH_SHORT).show()
-                    })
+                    getString(R.string.dialog_cancel), null
+                )
 
                 val dialog: AlertDialog = builder.create()
                 dialog.show()
-
             }
 
         }
@@ -67,11 +65,21 @@ class MainActivity : AppCompatActivity() {
 
 
     /**
-     * Esta función nos permite saber si los campos obligatorios estan vacios
+     * Esta función nos permite saber si el campo name y lasname estan vacio
+     * En caso de estar vacio. Establece el foco en el campo y muestra una advertencia
      */
     private fun validField(): Boolean {
         var isValid = true
 
+        if (binding.editTextLastName.text.toString().length == 0) {
+            binding.tilLastName.run {
+                error = getString(R.string.help_require)
+                requestFocus()
+            }
+            isValid = false
+        } else {
+            binding.tilLastName.error = null
+        }
 
         if (binding.editTextName.text.toString().length == 0) {
             binding.tilName.run {
@@ -83,12 +91,7 @@ class MainActivity : AppCompatActivity() {
             binding.tilName.error = null
         }
 
-
-
-
-
         return isValid
-
     }
 
 
